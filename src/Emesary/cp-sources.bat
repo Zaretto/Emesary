870be9f9a4928c4688df4fff517174e1f7ee8cfe
Richard Harrison <rjh@zaretto.com>
2018-12-07 17:11:15 +0100

    Added C++ version of Emesary.

---
+++ src/Emesary/cp-sources.bat
@@ -0,0 +1,9 @@
+copy Emesary\*.cs Emesary4
+copy Emesary\*.cs Emesary45
+copy Emesary\*.cs Emesary46
+
+copy Emesary\Properties\*.cs Emesary4\Properties
+copy Emesary\Properties\*.cs Emesary45\Properties
+copy Emesary\Properties\*.cs Emesary46\Properties
+
+
